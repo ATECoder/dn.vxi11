d53f5e0c40b6ad01fd1dbb5c516f5d30a29f0cf3
David Hary <791370+ATECoder@users.noreply.github.com>
2023-01-09 12:08:30 -0800

    IEEE 488 Server test program: change demo to VXI-11 IEEEE 488.

--- src/ieee488/ieee488.TestServer/Program.cs
+++ src/ieee488/ieee488.TestServer/Program.cs
@@ -1,6 +1,6 @@
 // See https://aka.ms/new-console-template for more information
 
-Console.WriteLine( "Starting VXI-11 Demo Server..." );
+Console.WriteLine( "Starting VXI-11 IEEE 488 Server..." );
 try
 {
     cc.isr.VXI11.IEEE488.Ieee488Server server = new ();
@@ -10,10 +10,8 @@ try
 }
 catch ( System.Exception e )
 {
-    Console.WriteLine( "demoServer oops:" );
-    Console.WriteLine( e.Message );
-    Console.WriteLine( e.StackTrace );
+    Console.WriteLine( e.ToString() );
 }
 //server.stopRpcProcessing();// stop the service
-Console.WriteLine( "demoServer stopped." );
+Console.WriteLine( "VXI-11 IEEE 488 Server stopped." );
 
