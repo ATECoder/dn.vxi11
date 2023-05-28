cabcf73dd34d5bcdc85c554c3dffe8028036501a
David Hary <791370+ATECoder@users.noreply.github.com>
2023-05-27 21:06:06 -0700

    update packages.

--- src/items/version.build.bat
+++ src/items/version.build.bat
@@ -1,4 +1,4 @@
-"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\TextTransform.exe" -a !!build!true "version.build.tt"
+"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\TextTransform.exe" "version.build.tt" -a !!build!true
 @echo off
 for /f "tokens=*" %%s in (version.build.props) do (
   echo %%s
