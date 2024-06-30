68a60266d4f3c16b63de9e8154673e1db476f185
David Hary <791370+ATECoder@users.noreply.github.com>
2024-06-29 17:42:30 -0700

    Apply code analysis rules.

--- src/items/ProductKey.cs
+++ src/items/ProductKey.cs
@@ -4,5 +4,6 @@ namespace cc.isr.VXI;
 internal static partial class SolutionInfo
 {
     /// <summary> The public key. </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage( "Style", "IDE1006:Naming Styles", Justification = "<Pending>" )]
     public const string PublicKey = "0024000004800000940000000602000000240000525341310004000001000100dba29c0e6dba927c84e79ce36609bb871da7c63c8ef88addafa51ccc967604e03c21afeb0cb5f5e74028e1f151bf92e9fe73166bfab3a61c4db81e8dad98c020f06368cce665735e9a2e42741b80ea60a3dcc6b4ad212928baf132b99c0ce9a2d0eae0b14d4fb2b7d2e4d5eba898b61e1e69c1d47db03a92eb9324137084bffb";
 }
