e86938d49f2c1d2fce23d1ec49ab2fa3d03e4733
David Hary <791370+ATECoder@users.noreply.github.com>
2025-10-30 20:14:25 -0700

    - Settings:   - Override CreateScribe.   - Use Read Settings to initialize the settings instance.

--- src/vxi/vxi.mstest/MSTest/LocationSettings.cs
+++ src/vxi/vxi.mstest/MSTest/LocationSettings.cs
@@ -11,6 +11,13 @@ internal sealed class LocationSettings : cc.isr.Json.AppSettings.Settings.Locati
 {
     #region " singleton "
 
+    /// <summary>   Creates the scribe. </summary>
+    /// <remarks>   2025-10-30. </remarks>
+    public override void CreateScribe()
+    {
+        this.Scribe = new( [this.SectionName], [this] );
+    }
+
     /// <summary>   Creates an instance of the <see cref="LocationSettings"/> after restoring the
     /// application context settings to both the user and all user files. </summary>
     /// <remarks>   2023-05-15. </remarks>
@@ -20,31 +27,12 @@ internal sealed class LocationSettings : cc.isr.Json.AppSettings.Settings.Locati
         // Get the type of the class that declares this method.
         Type declaringType = System.Reflection.MethodBase.GetCurrentMethod()!.DeclaringType!;
 
-        // Get the AssemblyFileInfo for the assembly that contains the declaring type and
-        // append '.Settings' to the assembly name to build the JSon settings file name.
-
-        AssemblyFileInfo ai = new( declaringType.Assembly, null, ".Settings", ".json" );
-
-        // copy application context settings if files do not exist or to clear corrupted settings.
-
-        AppSettingsScribe.InitializeSettingsFiles( ai, System.Diagnostics.Debugger.IsAttached, System.Diagnostics.Debugger.IsAttached );
-
-        // read the settings using the default serializer and document options.
-
         LocationSettings ti = new()
         {
-            FilePath = ai.AllUsersAssemblyFilePath ?? ai.ThisUserAssemblyFilePath ?? ai.AppContextAssemblyFilePath ?? string.Empty,
             SectionName = declaringType.Name
         };
 
-        ti.Scribe = new( [ti.SectionName], [ti], ai );
-
-        ti.FilePath = ti.Scribe.UserSettingsPath;
-
-        ti.ReadSettings();
-
-        if ( !ti.Scribe.SettingsExist( ti.FilePath, out string details ) )
-            throw new InvalidOperationException( $"Failed reading the settings; {details}" );
+        ti.ReadSettings( declaringType, ".Settings", System.Diagnostics.Debugger.IsAttached, System.Diagnostics.Debugger.IsAttached );
 
         return ti;
     }
